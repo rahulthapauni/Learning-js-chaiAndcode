51ecdcf0a77918d61e641280418686a223d5f91b
Rahul Thapa <125943660+rahulthapauni@users.noreply.github.com>
2025-06-24 17:48:34 +0000

    datatype

---
+++ 01JsPart01/01_basics/02_dataype.js
@@ -0,0 +1,31 @@
+"use state"; //treat all code as newer JS version
+// alert("helooo")
+// alert(3+4);//popup available in browser
+
+// react, node mostly avoid semi-colon
+
+//Js ignores Spaces but keep the code readable
+console.log(6+
+    6
+)
+console.log(6+6)
+//both will work but second is much more and easier to read
+
+
+//datatypes
+
+//number
+//bigInt 
+//string => "" '' ``
+//boolean => true, false
+//null => standalone value ( know the type but need to show the intentational absence of value )
+//undefined => dont know type and value
+//symbol => unique ( to identify component which is unique ( figma tools , react ))
+
+
+//Non Primitive 
+//Objects 
+
+console.log( typeof "rahul");
+console.log(typeof null)
+console.log(typeof undefined)
