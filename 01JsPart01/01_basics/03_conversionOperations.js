30e6602753d95cf6ec6871e1f86714ab37789c36
Rahul Thapa <125943660+rahulthapauni@users.noreply.github.com>
2025-06-24 18:05:39 +0000

    learn Datatype-conversion

---
+++ 01JsPart01/01_basics/03_conversionOperations.js
@@ -0,0 +1,34 @@
+// let score = 33;
+// let scorest = '33';
+// let numAlpha = '33abc';
+// console.log(typeof score);
+// console.log(typeof(scorest));
+
+// let conScore = Number(scorest) //numeric strint -> number
+// console.log(typeof(conScore), conScore); 
+// let conAlpha = Number(numAlpha) //numericalpha -> Nan
+// console.log(typeof(conAlpha), conAlpha);
+
+// let nullVal = null;
+// let conNull = Number(nullVal)
+// console.log(typeof(conNull), conNull); //null -> 0
+
+//  nullVal = "";
+//  conNull = Number(nullVal)
+// console.log(typeof(conNull), conNull); //"" -> 0
+
+//  nullVal = false;
+//  conNull = Number(nullVal)
+// console.log(typeof(conNull), conNull); //false -> 0
+
+// let isLoggedIn = undefined;
+// let boolIsLoggedIn =Boolean(isLoggedIn)
+// console.log(typeof boolIsLoggedIn, boolIsLoggedIn) 
+// //nonZero ( negaive, positive) , "String",  -> true
+// //null, 0 , emptyString, undefined -> false
+
+
+// let number = 33
+// let strNum = String(number);
+// console.log(typeof strNum, strNum)
+
